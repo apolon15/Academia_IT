4fbe2aeb261b7bb8696c26ced6680556bc613281
apolon15 <ya.apolon15@yandex.ru>
2022-03-17 18:16:36 +0300

    Добить 5 case

--- src/com/academia/Academia_IT.java
+++ src/com/academia/Academia_IT.java
@@ -3,10 +3,7 @@ package com.academia;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Academia_IT {
     public static void main(String[] args) throws IOException {
@@ -21,8 +18,8 @@ public class Academia_IT {
         }
         System.out.println(num);
         do {
-            System.out.println("Добавить эл-т в список: 1 \nУдалить эл-т из списка: 2 " +
-                    "\nПоказать содержимое списка: 3 \nПроверить значение в списке: 4 \nЗаменить значене в списке: 5");
+            System.out.println("Добавить эл-т в список: 1 \nУдалить порядковый номер из списка: 2 " +
+                    "\nПоказать содержимое списка: 3 \nПроверить значение в списке: 4 \nЗаменить значение в списке: 5");
             int n;
             Iterator<Integer> it = num.iterator();
             switch (Integer.parseInt(bf.readLine())) {
@@ -35,7 +32,7 @@ public class Academia_IT {
                 case 2:
                     System.out.println("Введи число");
                     n = sc.nextInt();
-                    num.remove(n);
+                    num.remove(n - 1);
                     System.out.println(num);
                     break;
                 case 3:
@@ -45,41 +42,52 @@ public class Academia_IT {
                     System.out.println("Введи число");
                     n = sc.nextInt();
                     while (it.hasNext()) {
-                        if (n == it.next()) {
-                            System.out.println("Есть такое значение");
-                        } else if (!it.hasNext()) {
+                        try {
+                            if (n == it.next()) {
+                                System.out.println("Есть такое значение");
+                            } else if
+                            (!it.hasNext() && n != it.next()) {
+                            }
+                        } catch (NoSuchElementException ex) {
                             System.out.println("Такого значения нет");
                         }
                     }
                     break;
                 case 5:
-                    System.out.println("Заменить значение-1 / Заменить порядковый номер-2");
-                    switch (Integer.parseInt(bf.readLine())) {
-                        case 1:
-                            System.out.println("Введи число");
-                            n = sc.nextInt();
-                            while (it.hasNext()) {
-                                if (n == it.next()) {
-                                    num.remove(n);
-                                } else if (!it.hasNext()) {
-                                    System.out.println("Такого значения нет");
+                    boolean stop = false;
+                    do {
+                        System.out.println("Заменить значение-1 / Заменить порядковый номер-2");
+                        switch (Integer.parseInt(bf.readLine())) {
+                            case 1:
+                                int counter = -1;
+                                System.out.println("Введи число");
+                                n = sc.nextInt();
+                                System.out.println("На какое число заменить?");
+                                int change = sc.nextInt();
+                                while (it.hasNext()) {
+                                    counter++;
+                                    if (n == it.next()) {
+                                        num.remove(counter);
+                                        num.add(counter, change);
+                                    } else if (!it.hasNext()) {
+                                        System.out.println("Такого значения нет");
+                                    }
                                 }
-                            }
-                            break;
-                        case 2:
-                            System.out.println("Введи порядковый номер");
-                            n = sc.nextInt();
-                            System.out.println("Введи значение ");
-                            num.add(n, Integer.parseInt(bf.readLine()));
-                            break;
-                        default:
-                            System.out.println(55);
-                    }
+                                System.out.println(num);
+                                break;
+                            case 2:
+                                System.out.println("Введи порядковый номер");
+                                n = sc.nextInt();
+                                System.out.println("Введи значение ");
+                                num.add(n, Integer.parseInt(bf.readLine()));
+                                System.out.println(num);
+                                break;
+                            default:
+                        }
+                    } while (stop);
                 default:
-                    System.out.println("end");
+                    System.out.println("Выход из программы-0 / для продолжения нажми любую клавишу");
             }
-            System.out.println("Выход из программы-0 / для продолжения нажми любую клавишу");
-        }
-              while (sc.next().equals("0"));
+        } while (!sc.next().equals("0"));
     }
 }
