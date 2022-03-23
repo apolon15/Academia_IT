edf8f0d8acd3c104dd217ef2056e4b320a6a5dda
apolon15 <ya.apolon15@yandex.ru>
2022-03-23 16:41:14 +0300

    5 таска к домашке 30 числа

--- src/com/academia/Spisok.java
+++ src/com/academia/Spisok.java
@@ -3,13 +3,17 @@ package com.academia;
 import sun.awt.image.ImageWatched;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedList;
 
 public class Spisok {
     public static void main(String[] args) {
         // System.out.println(set200());
         //System.out.println(llString());
-        System.out.println(doubleNum());
+        //  System.out.println(doubleNum());
+        //System.out.println(del20());
+        System.out.println( reverseList());
     }
 
     public static LinkedList<Integer> set200() {
@@ -57,6 +61,36 @@ public class Spisok {
             arrList.set(i, (int) Math.pow(arrList.get(i), 2));
         }
         return arrList;
+    }
+
+    public static LinkedList<Integer> del20() {
+        LinkedList<Integer> l = new LinkedList<Integer>();
+        int i = 0;
+        while (i < 10) {
+            l.add(((int) (Math.random() * (30 - 15) + 15)));
+            i++;
+        }
+        System.out.println(l);
+        while (l.contains(20)) {
+            for (i = 0; i < l.size(); i++) {
+                if (l.get(i) == 20) {
+                    l.remove(i);
+                }
+            }
+        }
+        return l;
+    }
+
+    public static LinkedList<Integer> reverseList() {
+        LinkedList<Integer> l = new LinkedList<Integer>();
+        int i = 0;
+        while (i < 10) {
+            l.add(((int) (Math.random() * (30 - 15) + 15)));
+            i++;
+        }
+        System.out.println(l);
+        Collections.reverse(l);
+        return l;
     }
 
 
