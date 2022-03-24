2e8c167befdb35bc148ba7dd2a7e976f6e9144a7
apolon15 <ya.apolon15@yandex.ru>
2022-03-24 15:30:24 +0300

    5решена домашка

--- src/com/academia/Spisok.java
+++ src/com/academia/Spisok.java
@@ -2,21 +2,22 @@ package com.academia;
 
 import sun.awt.image.ImageWatched;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Spisok {
     public static void main(String[] args) {
-        // System.out.println(set200());
-        //System.out.println(llString());
-        //  System.out.println(doubleNum());
-        //System.out.println(del20());
-        System.out.println( reverseList());
+        // System.out.println(task1());
+        //System.out.println(task2());
+        //  System.out.println(task3());
+        //System.out.println(task4());
+        // System.out.println(task5());
+        //  System.out.println(task6());
+        // System.out.println(task7());
+       // task8();
+       // task9();
     }
 
-    public static LinkedList<Integer> set200() {
+    public static LinkedList<Integer> task1() {
         LinkedList<Integer> l = new LinkedList<Integer>();
         int i = 0;
         while (i < 10) {
@@ -30,7 +31,7 @@ public class Spisok {
         return l;
     }
 
-    public static LinkedList<String> llString() {
+    public static LinkedList<String> task2() {
         LinkedList<String> l = new LinkedList<String>();
         int i = 0;
         while (i < 10) {
@@ -49,7 +50,7 @@ public class Spisok {
         return l;
     }
 
-    public static ArrayList<Integer> doubleNum() {
+    public static ArrayList<Integer> task3() {
         ArrayList<Integer> arrList = new ArrayList<>(10);
         int i = 0;
         while (i < 10) {
@@ -63,7 +64,7 @@ public class Spisok {
         return arrList;
     }
 
-    public static LinkedList<Integer> del20() {
+    public static LinkedList<Integer> task4() {
         LinkedList<Integer> l = new LinkedList<Integer>();
         int i = 0;
         while (i < 10) {
@@ -81,7 +82,7 @@ public class Spisok {
         return l;
     }
 
-    public static LinkedList<Integer> reverseList() {
+    public static LinkedList<Integer> task5() {
         LinkedList<Integer> l = new LinkedList<Integer>();
         int i = 0;
         while (i < 10) {
@@ -93,6 +94,98 @@ public class Spisok {
         return l;
     }
 
+    public static LinkedList<Integer> task6() {
+        LinkedList<Integer> l = new LinkedList<Integer>();
+        int i = 0;
+        while (i < 100) {
+            l.add(0);
+            i++;
+        }
+        System.out.println(l);
 
+        l.set(0, 1);
+        l.set(99, 1);
+        return l;
+    }
+
+    public static LinkedList<Integer> task7() {
+        LinkedList<Integer> l = new LinkedList<Integer>();
+        int i = 0;
+        l.add(0);
+        while (i < 45) {
+            l.add(l.get(i) + 2);
+            i++;
+        }
+        return l;
+    }
+
+    public static void task8() {
+        Scanner sc = new Scanner(System.in);
+        LinkedList<Integer> l = new LinkedList<Integer>();
+        int sum = 10;
+        while (sum != 0) {
+            l.add((int) (Math.random() * 21));
+            sum--;
+        }
+        int mult = 1;
+        System.out.println(l);
+        System.out.println("Введи число");
+        int numbr = sc.nextInt();
+        if (l.contains(numbr))
+            System.out.println(7145);
+        else System.out.println(5147);
+        for (int num : l) {
+            sum += num;
+            mult *= num;
+        }
+        System.out.println("Сумма ячеек = " + sum + "; произведение ячеек = " + mult);
+    }
+
+
+    public static void task9() {
+        List<Integer> l = new ArrayList<>(10);
+        int max = 0;
+        int min = 20;
+        int i = 0;
+        int indmax = 0;
+        int indmin = -1;
+        while (i < 10) {
+            l.add((int) (Math.random() * 21));
+            i++;
+        }
+        System.out.println(l);
+        for (i = 0; i < l.size(); i++) {
+            if (max < l.get(i)) {
+                indmax = i;
+                max = l.get(i);
+            }
+            if (min > l.get(i)) {
+                min = l.get(i);
+                indmin = i;
+            }
+            if (i == l.size() - 1) {
+                l.set(indmax, min);
+                l.set(indmin, max);
+            }
+        }
+        System.out.println(l);
+        System.out.println("max = " + max + " min = " + min);
+        List<Integer> copyL = l;
+        System.out.print("Повторяемые числа ");
+        Iterator<Integer> it = copyL.iterator();
+        min = 0;
+        while (it.hasNext()) {
+            max = it.next();
+            it.remove();
+            if (l.contains(max)) {
+// System.out.print(max+"; ");
+                System.out.printf("%d; ", max);
+                min++;
+            }
+        }
+        if (min == 0) {
+            System.out.print("отсутствуют");
+        }
+    }
 }
 
