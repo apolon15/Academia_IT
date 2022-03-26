ed2d6fb566e295746ef00a2526378cdbf3bed13e
apolon15 <ya.apolon15@yandex.ru>
2022-03-26 16:57:12 +0300

    Переделал 2 таск

--- src/com/academia/Academia_IT.java
+++ src/com/academia/Academia_IT.java
@@ -7,93 +7,102 @@ import java.util.*;
 
 public class Academia_IT {
     public static void main(String[] args) throws IOException {
-          collection();
+        collection();
         //collectionUsers();
     }
 
     public static void collection() throws IOException {
-        System.out.println(" Введи  размерность коллекции: ");
         Scanner sc = new Scanner(System.in);
-        int sizeList = sc.nextInt();
-        List<Integer> num = new ArrayList<>(sizeList);
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Введи " + sizeList + " чисел");
-        for (int i = 0; i < sizeList; i++) {
-            num.add(Integer.parseInt(bf.readLine()));
-        }
-        System.out.println(num);
+        boolean stop = false;
         do {
-            System.out.println("Добавить эл-т в список: 1 \nУдалить порядковый номер из списка: 2 " +
-                    "\nПоказать содержимое списка: 3 \nПроверить значение в списке: 4 \nЗаменить значение в списке: 5");
-            int n;
-            int change;
-            Iterator<Integer> it = num.iterator();
-            switch (Integer.parseInt(bf.readLine())) {
-                case 1:
-                    System.out.println("Введи число");
-                    n = sc.nextInt();
-                    num.add(n);
-                    System.out.println(num);
-                    break;
-                case 2:
-                    System.out.println("Введи число");
-                    n = sc.nextInt();
-                    try {
-                        num.remove(n - 1);
+            try {
+                BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+                System.out.println(" Введи  размерность коллекции: ");
+                int sizeList = Integer.parseInt(bf.readLine());
+                //  int sizeList = sc.nextInt();
+                List<Integer> num = new ArrayList<Integer>(sizeList);
+                System.out.println("Введи " + sizeList + " чисел");
+                for (int i = 0; i < sizeList; i++) {
+                    num.add(Integer.parseInt(bf.readLine()));
+                }
+                System.out.println(num);
+                System.out.println("Добавить эл-т в список: 1 \nУдалить порядковый номер из списка: 2 " +
+                        "\nПоказать содержимое списка: 3 \nПроверить значение в списке: 4 \nЗаменить значение в списке: 5");
+                int n;
+                int change;
+                Iterator<Integer> it = num.iterator();
+                switch (Integer.parseInt(bf.readLine())) {
+                    case 1:
+                        System.out.println("Введи число");
+                        n = sc.nextInt();
+                        num.add(n);
                         System.out.println(num);
-                    } catch (IndexOutOfBoundsException ex) {
-                        System.out.println("Внимательно читай условия.Нет такого порядкового  номера. Выход из программы-0 / для продолжения нажми любую клавишу");
-                    }
-                    break;
-                case 3:
-                    System.out.println(num);
-                    break;
-                case 4:
-                    System.out.println("Введи число");
-                    n = sc.nextInt();
-                    while (it.hasNext()) {
+                        break;
+                    case 2:
+                        System.out.println("Введи число");
+                        n = sc.nextInt();
                         try {
-                            if (n == it.next()) {
-                                System.out.println("Есть такое значение");
-                            } else if
-                            (!it.hasNext() && n != it.next()) {
-                            }
-                        } catch (NoSuchElementException ex) {
-                            System.out.println("Такого значения нет");
+                            num.remove(n - 1);
+                            System.out.println(num);
+                        } catch (IndexOutOfBoundsException ex) {
+                            System.out.println("Внимательно читай условия.Нет такого порядкового  номера.");
                         }
-                    }
-                    break;
-                case 5:
-                    System.out.println("Заменить значение-1 / Заменить порядковый номер-2");
-                    switch (Integer.parseInt(bf.readLine())) {
-                        case 1:
-                            System.out.println("Введи число");
-                            n = sc.nextInt();
-                            System.out.println("На какое число заменить?");
-                            change = sc.nextInt();
-                            String index = String.valueOf(num.indexOf(n));
+                        break;
+                    case 3:
+                        System.out.println(num);
+                        break;
+                    case 4:
+                        System.out.println("Введи число");
+                        n = sc.nextInt();
+                        while (it.hasNext()) {
                             try {
-                                num.set(Integer.parseInt(String.valueOf(num.indexOf(n))), change);
-                            } catch (IndexOutOfBoundsException ex) {
+                                if (n == it.next()) {
+                                    System.out.println("Есть такое значение");
+                                } else if
+                                (!it.hasNext() && n != it.next()) {
+                                }
+                            } catch (NoSuchElementException ex) {
                                 System.out.println("Такого значения нет");
                             }
-                            System.out.println(num);
-                            break;
-                        case 2:
-                            System.out.println("Введи порядковый номер");
-                            n = sc.nextInt();
-                            System.out.println("Введи значение ");
-                            change = Integer.parseInt(bf.readLine());
-                            num.set(n - 1, change);
-                            System.out.println(num);
-                            break;
-                        default:
-                    }
-                default:
-                    System.out.println("Выход из программы-0 / для продолжения нажми любую клавишу");
+                        }
+                        break;
+                    case 5:
+                        System.out.println("Заменить значение-1 / Заменить порядковый номер-2");
+                        switch (Integer.parseInt(bf.readLine())) {
+                            case 1:
+                                System.out.println("Введи число");
+                                n = sc.nextInt();
+                                System.out.println("На какое число заменить?");
+                                change = sc.nextInt();
+                                String index = String.valueOf(num.indexOf(n));
+                                try {
+                                    while (num.contains(n)) {
+                                        num.set(Integer.parseInt(String.valueOf(num.indexOf(n))), change);
+                                    }
+                                } catch (IndexOutOfBoundsException ex) {
+                                    System.out.println("Такого значения нет");
+                                }
+                                System.out.println(num);
+                                break;
+                            case 2:
+                                System.out.println("Введи порядковый номер");
+                                n = sc.nextInt();
+                                System.out.println("Введи значение ");
+                                change = Integer.parseInt(bf.readLine());
+                                num.set(n - 1, change);
+                                System.out.println(num);
+                                break;
+                        }
+                    default:
+                }
+            } catch (IllegalArgumentException ex) {
+                System.out.println("Значение должно быть более 0 либо введен \"пробел\"");
+            } catch (InputMismatchException ex) {
+                System.out.println("Введено не число");
             }
+            System.out.println("Выход из программы-0 / Начать заново нажми любую клавишу");
         }
-        while (!sc.next().equals("0"));
+        while (!sc.next().equals("0") || !stop);
     }
 
     public static void collectionUsers() throws IOException {
@@ -199,10 +208,4 @@ public class Academia_IT {
             }
         } while (!stop);
     }
-
-
-
-
-
-
 }
